f3eb41f0c2c8c33a2014dd761d41564cdd04f616
Bojan <10603459@mydbs.ie>
2023-09-18 22:40:47 +0100

    Adding image to Product model

--- website/models.py
+++ website/models.py
@@ -31,6 +31,7 @@ class Product(db.Model):
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
     price = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String(100), nullable=True)
     category_id = db.Column(db.Integer, db.ForeignKey(
         'category.id'), nullable=False)
     order_items = db.relationship('OrderItem', backref='product', lazy=True)
