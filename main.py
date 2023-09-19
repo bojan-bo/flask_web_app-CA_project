dea9a727df63da957036f9aaf4720afff1ad6dda
Bojan <10603459@mydbs.ie>
2023-09-19 20:38:02 +0100

    Adding stripe key

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from website import create_app
+import stripe
 
 app = create_app()
 
 app.config['STATIC_URL_PATH'] = '/static'
+
+stripe.api_key = app.config['STRIPE_SECRET_KEY']
 
 if __name__ == '__main__':
     app.run(debug=True)
