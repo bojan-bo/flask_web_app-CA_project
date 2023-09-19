59be0b546ff93ec5d8ec31c290bb8ba833ce9c2d
Bojan <10603459@mydbs.ie>
2023-09-19 20:37:12 +0100

    Adding logic for cart

--- website/views.py
+++ website/views.py
@@ -1,5 +1,4 @@
-from flask_login import login_user
-from flask import Blueprint, render_template, request, flash, redirect, session, url_for
+from flask import Blueprint, Flask, render_template, request, flash, redirect, session, url_for, current_app
 from flask_login import login_required, current_user
 from .models import User, Product, Category
 from .forms import ProductForm, PromoteForm
@@ -7,8 +6,12 @@ from .decorators import check_admin
 from . import db
 import os
 from werkzeug.utils import secure_filename
+from flask import render_template, request, redirect, url_for, flash
+import stripe
 
 views = Blueprint('views', __name__)
+
+app = Flask(__name__)
 
 
 def save_image(image):
@@ -25,7 +28,7 @@ def home():
 @views.route('/promote', methods=['GET', 'POST'])
 @login_required
 def promote():
-    if current_user.role('role') != 'admin':
+    if current_user.role != 'admin':
         flash('You do not have permission to access this page.', 'error')
         return redirect(url_for('views.home'))
 
@@ -76,6 +79,121 @@ def add_product():
     return render_template('add_product.html', form=form)
 
 
+@views.route('/add-to-cart/<int:product_id>')
+def add_to_cart(product_id):
+    product = Product.query.get_or_404(product_id)
+    cart = session.get('cart', [])
+    # Check if the item is already in the cart
+    for item in cart:
+        if item['id'] == product.id:
+            item['quantity'] = item.get('quantity', 0) + 1
+            break
+    else:
+        # Item is not in the cart, add it
+        cart.append({
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'quantity': 1,
+        })
+    session['cart'] = cart
+    session.modified = True
+    flash('Item added to cart', 'success')
+    return redirect(url_for('views.products', category_name=product.category.name))
+
+
+@views.route('/cart')
+def cart():
+    cart = session.get('cart', [])
+    total = sum(item['price'] * item.get('quantity', 1) for item in cart)
+    return render_template('cart.html', cart={'items': cart, 'total': total})
+
+
+@views.route('/remove-from-cart/<int:item_id>')
+def remove_from_cart(item_id):
+    cart = session.get('cart', [])
+    # find the item in the cart
+    item = next((item for item in cart if item['id'] == item_id), None)
+    if item:
+        # remove the item from the cart
+        cart.remove(item)
+        session['cart'] = cart
+        session.modified = True
+        flash('Item removed from cart', 'success')
+    else:
+        flash('Item not found in cart', 'error')
+    return redirect(url_for('views.cart'))
+
+
+@views.route('/checkout', methods=['GET', 'POST'])
+def checkout():
+    cart = session.get('cart', [])
+    amount = int(sum(item['price'] * item.get('quantity', 1)
+                 for item in cart) * 100)
+    intent = stripe.PaymentIntent.create(
+        amount=amount,
+        currency='eur',
+    )
+    return render_template('checkout.html', client_secret=intent.client_secret, stripe_public_key=current_app.config['STRIPE_PUBLIC_KEY'])
+
+
+@views.route('/charge', methods=['POST'])
+def charge():
+    cart = session.get('cart', [])
+    amount = int(sum(item['price'] * item.get('quantity', 1)
+                 for item in cart) * 100)  # amount in cents
+    payment_method_id = request.form.get('paymentMethodId')
+    if not payment_method_id:
+        flash('Payment failed. Please try again.', 'error')
+        return redirect(url_for('views.checkout'))
+
+    try:
+        # Create a payment intent
+        intent = stripe.PaymentIntent.create(
+            amount=amount,
+            currency='usd',
+            payment_method=payment_method_id,
+            confirmation_method='manual',
+            confirm=True,
+        )
+        flash('Payment was successful', 'success')
+        session['cart'] = []  # clear the cart
+        session.modified = True
+        return redirect(url_for('views.thank_you'))
+    except stripe.error.CardError as e:
+        # Since it's a decline, stripe.error.CardError will be caught
+        body = e.error.get('charge')
+        code = body.get('code')
+        flash(f'Card has been declined. Error code: {code}', 'error')
+    except stripe.error.RateLimitError as e:
+        # Too many requests made to the API too quickly
+        flash('Too many requests made to the API too quickly. Please try again later.', 'error')
+    except stripe.error.InvalidRequestError as e:
+        # Invalid parameters were supplied to Stripe's API
+        flash(
+            'Invalid parameters were supplied to Stripe\'s API. Please try again.', 'error')
+    except stripe.error.AuthenticationError as e:
+        # Authentication with Stripe's API failed
+        flash('Authentication with Stripe\'s API failed. Please try again.', 'error')
+    except stripe.error.APIConnectionError as e:
+        # Network communication with Stripe failed
+        flash('Network communication with Stripe failed. Please try again.', 'error')
+    except stripe.error.StripeError as e:
+        # Display a very generic error to the user, and maybe send
+        # yourself an email
+        flash('An error occurred while processing your payment. Please try again.', 'error')
+    except Exception as e:
+        # Something else happened, completely unrelated to Stripe
+        flash('An unexpected error occurred. Please try again.', 'error')
+
+    return redirect(url_for('views.checkout'))
+
+
+@views.route('/thank-you')
+def thank_you():
+    return render_template('thank_you.html')
+
+
 @views.route('/account')
 @login_required
 def account():
@@ -92,16 +210,3 @@ def admin():
 @views.route('/contact')
 def contact():
     return render_template('contact.html', user=current_user)
-
-
-@views.route('/cart')
-def cart():
-    cart = session.get('cart', [])
-    return render_template('cart.html', user=current_user, cart=cart)
-
-
-@views.route('/search')
-def search():
-    query = request.args.get('query')
-    products = Product.query.filter(Product.name.contains(query)).all()
-    return render_template('search.html', user=current_user, products=products)
